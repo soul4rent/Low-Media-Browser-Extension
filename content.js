5082cb0566137e50e279c863ce32eeff7367d425
soul4rent <kyle.a.wadsworth@gmail.com>
2018-06-21 14:18:15 -0700

    a very basic start

--- content.js
+++ content.js
@@ -1,1 +1,10 @@
-alert("extension test");
+var x = document.getElementsByTagName("img");
+
+for (i = 0; i<x.length-1; i++){
+  console.log(x[0]); //logging all images
+  //TODO: Edit image styles to be "none"
+  //TODO: Research how to get first frame of gif (replace .gif image with first frame)
+}
+
+
+//lets see if this removes images
